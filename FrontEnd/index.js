50f904a6832206c5dfa866ffd72de875bf4255ce
Nesanu <nicoleta_esanu@yahoo.fr>
2023-11-24 18:38:59 +0100

    Modifs du 24.11.23

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -7,34 +7,7 @@ async function getWorks() {
   worksList = await reponse.json();
   console.log("works", worksList);
 
-  // Recupérer la div gallery du fichier index.html:
-  // const gallery = document.querySelectorAll(".gallery");
-  let gallery = document.querySelector(".gallery");
-  console.log(gallery);
-
-  for (let i = 0; i < worksList.length; i++) {
-    console.log(worksList[i].title);
-    console.log(worksList[i].image);
-    // Créer la figure avec l'image, titre et ajouter en tant qu'enfant à gallery (append child)
-
-    let figureElement = document.createElement("figure");
-
-    let imageElement = document.createElement("img");
-    //let imageUrl = document.createElement("src")
-    //imageElement.setAttribute("src", worksList[i].imageUrl)
-    imageElement.src = worksList[i].imageUrl;
-
-    imageElement.setAttribute("alt", worksList[i].title);
-    //imageElement.innerText = worksList[i].title
-
-    let figCaption = document.createElement("figcaption");
-    figCaption.innerText = worksList[i].title;
-
-    figureElement.appendChild(imageElement);
-    figureElement.appendChild(figCaption);
-
-    gallery.appendChild(figureElement);
-  }
+  fillGallery(worksList);
 }
 
 getWorks();
@@ -53,7 +26,12 @@ async function getCategories() {
   let buttonElement = document.createElement("button");
   buttonElement.setAttribute("class", "btn-filtres");
   buttonElement.innerText = "Tous";
+
   filtres.appendChild(buttonElement);
+  buttonElement.addEventListener("click", () => {
+    console.log(categoriesList[i].name);
+    filterWorksByCategory(categoriesList[i].id);
+  });
 
   for (let i = 0; i < categoriesList.length; i++) {
     console.log(categoriesList[i].name);
@@ -73,78 +51,45 @@ async function getCategories() {
   }
 }
 
-function filterWorksByCategory(categoryId) {
-  console.log(categoryId);
-
-  let resultat = worksList.filter((work) => work.categoryId === categoryId);
-  console.log(resultat);
-
+function fillGallery(arrayOfWork) {
   let gallery = document.querySelector(".gallery");
   gallery.innerHTML = "";
-  console.log(gallery);
 
-  // buttonElement = categoriesList("Tous");
-  let buttonElement = document.createElement("button");
-  buttonElement.setAttribute("class", "btn-filtres");
-  buttonElement.innerText = "";
-  buttonElement.addEventListener("click", () => {
-    console.log(categoriesList[i].name);
-    filterWorksByCategory(categoriesList[i].id);
-    // filtres.appendChild(buttonElement);
-  });
-
-  for (let i = 0; i < resultat.length; i++) {
-    // console.log(resultat[i].title);
-    // console.log(resultat[i].image);
-
+  for (let i = 0; i < arrayOfWork.length; i++) {
     // Créer la figure avec l'image, titre et ajouter en tant qu'enfant à gallery (append child)
 
     let figureElement = document.createElement("figure");
 
     let imageElement = document.createElement("img");
 
-    imageElement.src = resultat[i].imageUrl;
+    imageElement.src = arrayOfWork[i].imageUrl;
 
-    imageElement.setAttribute("alt", resultat[i].name);
+    imageElement.setAttribute("alt", arrayOfWork[i].name);
 
     let figCaption = document.createElement("figcaption");
-    figCaption.innerText = resultat[i].title;
+    figCaption.innerText = arrayOfWork[i].title;
 
     figureElement.appendChild(imageElement);
     figureElement.appendChild(figCaption);
 
     gallery.appendChild(figureElement);
   }
+}
 
-  //    Creer le filter, actualiser l'affichage (regarder la fonction get works),
-  //   réutiliser le code de la fonction get works (copier-coller le code) ==> ok
+function filterWorksByCategory(categoryId) {
+  console.log(categoryId);
+
+  let resultat = worksList.filter((work) => work.categoryId === categoryId);
+  console.log(resultat);
+
+  fillGallery(resultat);
 }
 
 getCategories();
 
-async function postUsersLogin() {
-  const reponse = await fetch("http://localhost:5678/api/users/login");
-  const UsersLoginId = await reponse.json();
-  console.log("UsersLogin", UsersLoginId);
-
-  let contact = document.getElementById("contact");
-  console.log(contact);
-  const form = document.querySelector("form");
-}
-postUsersLogin();
-
 // CHAT GPT:Il faut ajouter des boutons ou des sélecteurs pour permettre aux utilisateurs
 // de choisir une catégorie.
 
-// function filtrerParCategorie(categorie) {
-//     const images = document.querySelectorAll('.image');
-//     images.forEach(image => {
-//       const cat = image.getAttribute('data-category');
-//       if (categorie === 'tous' || cat === categorie) {
-//         image.style.display = 'block';
-//       } else {
-//         image.style.display = 'none';
-//       }
 //     });
 //   }
 
@@ -156,33 +101,10 @@ postUsersLogin();
 //     filtrerParCategorie('catégorie1');
 //   });
 
-//   document.getElementById('bouton-catégorie2').addEventListener('click', () => {
-//     filtrerParCategorie('catégorie2');
-//   });
-
-//   // Pour le bouton "Tous les projets"
+//   // Pour le bouton "Tous"
 //   document.getElementById('bouton-tous').addEventListener('click', () => {
 //     filtrerParCategorie('tous');
 //   });
-
-// Appelez la fonction de filtrage initialement pour afficher toutes les images.
-
-// filtrerParCategorie('tous');
-
-// // CHAT GPT: Supposons que data contient les URLs des images récupérées depuis votre API
-// const imageUrls = ['url_image_1', 'url_image_2', 'url_image_3'];
-
-// // Supposons que galleryElement est l'élément DOM où vous souhaitez afficher vos images
-// const galleryElement = document.getElementById('votre_galerie_id');
-
-// // Parcourez les URLs des images et ajoutez-les à la galerie
-// imageUrls.forEach(url => {
-//   const imgElement = document.createElement('img'); // Créez un élément image
-//   imgElement.src = url; // Définissez l'URL de l'image
-//   galleryElement.appendChild(imgElement); // Ajoutez l'image à la galerie
-// });
-
-//
 
 // export function ajoutWorks() {
 //     const worksElements = document.querySelectorAll(".gallery");
---
+++ FrontEnd/login.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  console.log(event);
+
+  //   const name = document.getElementById("name").value;
+  //   window.alert(name); "name" non relevant dans ma mon fichier login.html
+
+  window.location.href = "index.html";
+  const email = document.getElementById("email").value;
+  window.alert(email);
+
+  const password = document.getElementById("password").value;
+  window.alert(password);
+
+  const reponse = await fetch("http://localhost:5678/api/users/login");
+  // const usersLogin = await reponse.json();
+});
+
+async function postLogin(url = "", donnees = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(donnees),
+  });
+  return response.json();
+}
+postData("http://localhost:5678/api/users/login", { userId }).then(
+  (donnees) => {
+    console.log(donnees);
+  }
+);
+
+// // Exemple d'implémentation pour une requête POST
+// async function postData(url = "", donnees = {}) {
+//   // Les options par défaut sont indiquées par *
+//   const response = await fetch(url, {
+//     method: "POST", // *GET, POST, PUT, DELETE, etc.
+//     mode: "cors", // no-cors, *cors, same-origin
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: "same-origin", // include, *same-origin, omit
+//     headers: {
+//       "Content-Type": "application/json",
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: "follow", // manual, *follow, error
+//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     body: JSON.stringify(donnees), // le type utilisé pour le corps doit correspondre à l'en-tête "Content-Type"
+//   });
+//   return response.json(); // transforme la réponse JSON reçue en objet JavaScript natif
+// }
+
+// postData("https://example.com/solution", { solution: 42 }).then((donnees) => {
+//   console.log(donnees); // Les données JSON analysées par l'appel `donnees.json()`
+// });
