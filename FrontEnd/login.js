52fb648e22519689139af014abf84412fc02b4bc
Nesanu <nicoleta_esanu@yahoo.fr>
2023-11-29 20:34:03 +0100

    Modifs login.js suite session du 29.11.23

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -6,28 +6,38 @@ form.addEventListener("submit", async (event) => {
   //   const name = document.getElementById("name").value;
   //   window.alert(name); "name" non relevant dans ma mon fichier login.html
 
-  window.location.href = "index.html";
+  // window.location.href = "index.html";
   const email = document.getElementById("email").value;
   window.alert(email);
 
   const password = document.getElementById("password").value;
   window.alert(password);
 
+  const idData = {
+    email: email,
+    password: password,
+  };
+
   const response = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
     headers: { "Content-Type": "application/json" },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: '{"email","password"}',
-    // body: JSON.stringify(email, password),
+    body: JSON.stringify(idData),
   });
-  await response.json();
 
-  window.localStorage.setItem("token", response.token);
-  console.log(token);
+  const data = await response.json();
+  console.log(data);
+
+  window.localStorage.setItem("token", data.token);
+  console.log(data.token);
+
+  // Créer une condition: si le token existe if ... else
+
+  let tokenOk = true;
+  if (tokenOk) {
+    console.log("The token exists");
+  } else {
+    window.alert("Wrong password");
+  }
 });
 
 // postData("http://localhost:5678/api/users/login", { userId }).then(
