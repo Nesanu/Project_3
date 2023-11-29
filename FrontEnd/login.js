6f74de5f04d90cce0829805428e4db57dcd14f08
Nesanu <nicoleta_esanu@yahoo.fr>
2023-11-29 12:22:41 +0100

    Code login.js du 29.11 avt session

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -24,10 +24,11 @@ form.addEventListener("submit", async (event) => {
     body: '{"email","password"}',
     // body: JSON.stringify(email, password),
   });
-  return response.json();
-});
+  await response.json();
 
-window.localStorage.setItem();
+  window.localStorage.setItem("token", response.token);
+  console.log(token);
+});
 
 // postData("http://localhost:5678/api/users/login", { userId }).then(
 //   (donnees) => {
@@ -42,26 +43,3 @@ window.localStorage.setItem();
 
 //   return response.json();
 // }
-
-// // Exemple d'implémentation pour une requête POST
-// async function postData(url = "", donnees = {}) {
-//   // Les options par défaut sont indiquées par *
-//   const response = await fetch(url, {
-//     method: "POST", // *GET, POST, PUT, DELETE, etc.
-//     mode: "cors", // no-cors, *cors, same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: "same-origin", // include, *same-origin, omit
-//     headers: {
-//       "Content-Type": "application/json",
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(donnees), // le type utilisé pour le corps doit correspondre à l'en-tête "Content-Type"
-//   });
-//   return response.json(); // transforme la réponse JSON reçue en objet JavaScript natif
-// }
-
-// postData("https://example.com/solution", { solution: 42 }).then((donnees) => {
-//   console.log(donnees); // Les données JSON analysées par l'appel `donnees.json()`
-// });
