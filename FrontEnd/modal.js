156a601d5127c436f4b959752e2d1f8748d06d45
Nesanu <nicoleta_esanu@yahoo.fr>
2023-12-29 13:14:56 +0100

    Modifs_code 29.12

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -4,8 +4,13 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closeBtn = document.getElementById("close-btn");
+
+var previousBtn = document.getElementById("prev-btn");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -27,17 +32,37 @@ window.onclick = function (event) {
 function displayToutesLesPhotos() {
   document.querySelector("#touteslesphotos").style.display = "block";
   document.querySelector("#form-add").style.display = "none";
+  document.querySelector("#modal-title").innerHTML = "Galerie Photos";
+  previousBtn.style.display = "none";
 }
 
-function displayToutesLesPhotos() {
+// function displayToutesLesPhotos() {
+//   document.querySelector("#touteslesphotos").style.display = "none";
+//   document.querySelector("#form-add").style.display = "block";
+// }
+
+function displayFormulaireAjoutPhoto() {
   document.querySelector("#touteslesphotos").style.display = "none";
   document.querySelector("#form-add").style.display = "block";
+  document.querySelector("#modal-title").innerHTML = "Ajout Photo";
+  previousBtn.style.display = "block";
 }
 
 const poubelle = document.createElement("i");
 poubelle.classList.add("fa-solid", "fa-trash-can");
 
 // function fillGallery(arrayOfWork) {
-// let gallery = document.querySelector("gallery_id");
-// gallery.innerHTML = "";
+//   let gallery = document.querySelector("gallery_id");
+//   gallery.innerHTML = "";
 // }
+
+displayToutesLesPhotos();
+
+async function getWorks() {
+  const reponse = await fetch("http://localhost:5678/api/works");
+  worksList = await reponse.json();
+  console.log("works", worksList);
+  let gallery = document.querySelector("gallery_id");
+  fillGallery(worksList);
+}
+getWorks();
